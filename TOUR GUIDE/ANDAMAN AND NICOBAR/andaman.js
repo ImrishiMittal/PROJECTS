20d00046fb95e21aaf6dc25a47eca294265a0ae1
Rishi Mitttal <rishi11mital@gmail.com>
2024-09-25 11:06:09 +0530

    Andaman and Nicobar compleated

--- TOUR GUIDE/ANDAMAN AND NICOBAR/andaman.js
+++ TOUR GUIDE/ANDAMAN AND NICOBAR/andaman.js
@@ -1,29 +1,29 @@
 document.getElementById("history").addEventListener("click",()=>{
-    window.open ("/ANDAMAN AND NICOBAR/Things/HISTORY/history.html")
+    window.location= "/ANDAMAN AND NICOBAR/Things/HISTORY/history.html"
 });
 document.getElementById("culture").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/CULTURE/culture.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/CULTURE/culture.html"
 })
 document.getElementById("tradition").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/TRADITON/tradition.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/TRADITON/tradition.html"
 })
 document.getElementById("tourism").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/TOURISM/tourism.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/TOURISM/tourism.html"
 })
 document.getElementById("food").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/FOOD/food.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/FOOD/food.html"
 })
 document.getElementById("dress").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/DRESS/dress.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/DRESS/dress.html"
 })
 document.getElementById("languages").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/LANGUAGES/languages.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/LANGUAGES/languages.html"
 })
 document.getElementById("travel").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/TRAVEL/travel.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/TRAVEL/travel.html"
 })
 document.getElementById("location").addEventListener("click",()=>{
-    window.open("/ANDAMAN AND NICOBAR/Things/GEOGRAPHICAL/geographical.html")
+    window.location="/ANDAMAN AND NICOBAR/Things/GEOGRAPHICAL/geographical.html"
 })
 
 const sliders = document.querySelectorAll(".slider");
