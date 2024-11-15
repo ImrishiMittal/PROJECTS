e3b2041080cf3e676b876e147e59c2ae9d8da15b
Rishi Mitttal <rishi11mital@gmail.com>
2024-11-15 22:26:13 +0530

    karnataka done

--- TOUR GUIDE/KARNATAKA/karnataka.js
+++ TOUR GUIDE/KARNATAKA/karnataka.js
@@ -1,32 +1,32 @@
 document.getElementById("history").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/HISTORY/history.html")
+    window.location = "/KARNATAKA/Things/HISTORY/history.html"
 })
 document.getElementById("culture").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/CULTURE/culture.html")
+    window.location = "/KARNATAKA/Things/CULTURE/culture.html"
 })
 document.getElementById("tradion").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/TRADITON/tradition.html")
+    window.location = "/KARNATAKA/Things/TRADITON/tradition.html"
 })
 document.getElementById("tourism").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/TOURISM/tourism.html")
+    window.location = "/KARNATAKA/Things/TOURISM/tourism.html"
 })
 document.getElementById("food").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/FOOD/food.html")
+    window.location = "/KARNATAKA/Things/FOOD/food.html"
 })
 document.getElementById("festival").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/FESTIVALS/festival.html")
+    window.location = "/KARNATAKA/Things/FESTIVALS/festival.html"
 })
 document.getElementById("dress").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/DRESS/dress.html")
+    window.location = "/KARNATAKA/Things/DRESS/dress.html"
 })
 document.getElementById("languages").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/LANGUAGES/languages.html")
+    window.location = "/KARNATAKA/Things/LANGUAGES/languages.html"
 })
 document.getElementById("travel").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/TRAVEL/travel.html")
+    window.location = "/KARNATAKA/Things/TRAVEL/travel.html"
 })
 document.getElementById("location").addEventListener("click",()=>{
-    window.open("/KARNATAKA/Things/GEOGRAPHICAL/geographical.html")
+    window.location = "/KARNATAKA/Things/GEOGRAPHICAL/geographical.html"
 });
 const sliders = document.querySelectorAll(".slider");
 
