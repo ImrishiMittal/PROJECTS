216e2351512e33f0664ceec7f10a724a9e0b8d41
Rishi Mitttal <rishi11mital@gmail.com>
2024-11-17 19:45:10 +0530

    ladakh compleated

--- TOUR GUIDE/LADAKH/ladakh.js
+++ TOUR GUIDE/LADAKH/ladakh.js
@@ -1,32 +1,32 @@
 document.getElementById("history").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/HISTORY/history.html")
+    window.location ="/LADAKH/Things/HISTORY/history.html"
 })
 document.getElementById("culture").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/CULTURE/culture.html")
+    window.location ="/LADAKH/Things/CULTURE/culture.html"
 })
 document.getElementById("tradion").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/TRADITON/tradition.html")
+    window.location ="/LADAKH/Things/TRADITON/tradition.html"
 })
 document.getElementById("tourism").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/TOURISM/tourism.html")
+    window.location ="/LADAKH/Things/TOURISM/tourism.html"
 })
 document.getElementById("food").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/FOOD/food.html")
+    window.location ="/LADAKH/Things/FOOD/food.html"
 })
 document.getElementById("festival").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/FESTIVALS/festival.html")
+    window.location ="/LADAKH/Things/FESTIVALS/festival.html"
 })
 document.getElementById("dress").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/DRESS/dress.html")
+    window.location ="/LADAKH/Things/DRESS/dress.html"
 })
 document.getElementById("languages").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/LANGUAGES/languages.html")
+    window.location ="/LADAKH/Things/LANGUAGES/languages.html"
 })
 document.getElementById("travel").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/TRAVEL/travel.html")
+    window.location ="/LADAKH/Things/TRAVEL/travel.html"
 })
 document.getElementById("location").addEventListener("click",()=>{
-    window.open("/LADAKH/Things/GEOGRAPHICAL/geographical.html")
+    window.location ="/LADAKH/Things/GEOGRAPHICAL/geographical.html"
 });
 const sliders = document.querySelectorAll(".slider");
 
