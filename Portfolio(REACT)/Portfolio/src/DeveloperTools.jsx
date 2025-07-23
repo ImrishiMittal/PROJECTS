ad8763b5fd11999fd2fcbcb39e6b418f8008ed3f
Rishi Mitttal <rishi11mital@gmail.com>
2025-07-23 12:10:32 +0530

    Dev tools done

--- Portfolio(REACT)/Portfolio/src/DeveloperTools.jsx
+++ Portfolio(REACT)/Portfolio/src/DeveloperTools.jsx
@@ -4,9 +4,18 @@ function DeveloperTools(){
         <>
         <h4 className={styles.h4}>DEVELOPER TOOLS</h4>
         <div className={styles.cards}>
-        <div className={styles.skillsLogo}></div>
-        <div className={styles.skillsLogo}></div>
-        <div className={styles.skillsLogo}></div>
+        <div className={styles.skillsLogo}>
+            <img src="./image/git and github.webp" alt="" />
+            <p className={styles.para}>GIT AND GITHUB</p>
+        </div>
+        <div className={styles.skillsLogo}>
+            <img src="./image/vsc.png" alt="" />
+            <p className={styles.para}>VS CODE</p>
+        </div>
+        <div className={styles.skillsLogo}>
+        <img src="./image/figma.png" alt="" />
+            <p className={styles.para}>FIGMA</p>
+        </div>
         </div>
         </>
     )
